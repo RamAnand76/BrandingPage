f33711e47d0e0d218949a67c5658cb02ce80570d
Ramanand R <ramuttenravitsr2002@gmail.com>
2025-08-19 19:31:55 +0000

    in dicover our products place the arrow right to text.
    Also ensure code

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -78,7 +78,7 @@ const Index = () => {
             </Link>
             <Link href="/products" className="w-full sm:w-auto">
                 <ShinyButton className="w-full button-gradient text-white">
-                  Discover Our Products <ArrowRight className="ml-2 w-4 h-4" />
+                  Discover Our Products <ArrowRight className="w-4 h-4" />
                 </ShinyButton>
             </Link>
           </motion.div>
