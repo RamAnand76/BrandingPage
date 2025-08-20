357db8882399b321f3e4552a681bb51dea310bb8
Ramanand R <ramuttenravitsr2002@gmail.com>
2025-08-20 16:18:27 +0000

    fix: Resolve the deplpyement issue [21:44:25.503] Running build in Washi

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c06f7398e182f6e2"; }
+/******/ 		__webpack_require__.h = function() { return "093afe6c6910a380"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
