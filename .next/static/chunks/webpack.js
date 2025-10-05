ee32fab16484e5199e114ec7fcd98f405d84cb0f
Ramanand R <ramuttenravitsr2002@gmail.com>
2025-10-05 13:38:29 +0000

    But instead of the border the star is inside teh card.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0b2512239fcef734"; }
+/******/ 		__webpack_require__.h = function() { return "614dfb626da60fe2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
