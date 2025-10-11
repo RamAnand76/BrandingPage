8f7324ec4ea092950535d5bfbb76185cf5ea625f
Ramanand R <ramuttenravitsr2002@gmail.com>
2025-10-11 12:20:15 +0000

    INtegrated it but the content should like the following: About RE:INVENT

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("fa1d905091e5f309")
+/******/ 		__webpack_require__.h = () => ("a0227f20a2d72353")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
@@ -163,6 +163,11 @@
 /******/ 			var r = fn();
 /******/ 			return r === undefined ? result : r;
 /******/ 		}
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	(() => {
+/******/ 		__webpack_require__.nc = undefined;
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/require chunk loading */
