6588ba2c6939240b980f8578ccfe8971ed2c3fbc
Ramanand R <ramuttenravitsr2002@gmail.com>
2025-10-30 09:54:48 +0000

    I can't see the background proerly

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1bc0b81850869ea7"; }
+/******/ 		__webpack_require__.h = function() { return "41eacb93cdadae80"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
