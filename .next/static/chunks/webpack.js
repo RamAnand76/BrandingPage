abb6097ca05d54f5ae93d78a6d155ea0d6f7b158
Ramanand R <ramuttenravitsr2002@gmail.com>
2025-10-30 13:04:42 +0000

    Instead of this current Feature tabs and the image use the following com

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ae0982673b413589"; }
+/******/ 		__webpack_require__.h = function() { return "092b1cd35065ff68"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
