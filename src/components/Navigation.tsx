f15919422e4c26da3775c6254ad4b6bda17d7ca6
Ramanand R <ramuttenravitsr2002@gmail.com>
2025-10-31 07:25:52 +0000

    CHange it to "Re-Invent" with color green (_for element src/app/page.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { FloatingNav } from "@/components/ui/floating-navbar";
@@ -6,7 +5,7 @@ import { Briefcase, Home, Info, Mail, AppWindow, Tag } from "lucide-react";
 
 const Navigation = () => {
   const navItems = [
-    { name: "Home", link: "/", icon: <Home className="h-4 w-4 text-neutral-500 dark:text-white" /> },
+    { name: <span style={{color: 'hsl(var(--primary))'}}>Re-Invent</span>, link: "/", icon: <Home className="h-4 w-4 text-neutral-500 dark:text-white" /> },
     { name: "Services", link: "/#features", icon: <Briefcase className="h-4 w-4 text-neutral-500 dark:text-white" /> },
     { name: "Products", link: "/products", icon: <AppWindow className="h-4 w-4 text-neutral-500 dark:text-white" /> },
     { name: "About", link: "/#about", icon: <Info className="h-4 w-4 text-neutral-500 dark:text-white" /> },
