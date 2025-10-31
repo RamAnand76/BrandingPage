097e80c9f3c27813c2d594c2497b3a75374efb38
Ramanand R <ramuttenravitsr2002@gmail.com>
2025-10-31 07:53:21 +0000

    I have changed the logo but still it's not updated in the navbar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "953622f2d247c540"; }
+/******/ 		__webpack_require__.h = function() { return "c23954ae49086ebd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
