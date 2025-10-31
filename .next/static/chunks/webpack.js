a577a63eb975a629bb15aba9317562fb5b3dfc58
Ramanand R <ramuttenravitsr2002@gmail.com>
2025-10-31 07:56:59 +0000

    This is how the logo.png look like but in the rendered page it's not lik

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c23954ae49086ebd"; }
+/******/ 		__webpack_require__.h = function() { return "eaab2f96671e310d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -7,8 +7,8 @@ import Image from "next/image";
 const Navigation = () => {
   const navItems = [
     { name: <div className="flex items-center gap-2">
-              <Image src="/lovable-uploads/logo.png" width={24} height={24} alt="RE:INVENT Logo" />
-            </div>, link: "/", icon: <Image src="/lovable-uploads/logo.png" width={28} height={28} alt="RE:INVENT Logo" /> },
+              <Image src="/lovable-uploads/logo.png" width={120} height={32} alt="RE:INVENT Logo" />
+            </div>, link: "/", icon: <Image src="/lovable-uploads/logo.png" width={120} height={32} alt="RE:INVENT Logo" /> },
     { name: "Services", link: "/#features", icon: <Briefcase className="h-4 w-4 text-neutral-500 dark:text-white" /> },
     { name: "Products", link: "/products", icon: <AppWindow className="h-4 w-4 text-neutral-500 dark:text-white" /> },
     { name: "About", link: "/#about", icon: <Info className="h-4 w-4 text-neutral-500 dark:text-white" /> },
