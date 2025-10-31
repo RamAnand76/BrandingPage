6a91015e0128a42b56b75cd5feaadf0549089483
Ramanand R <ramuttenravitsr2002@gmail.com>
2025-10-31 05:46:33 +0000

    Highlight the Brand name (_for element src/app/page.tsx > h2_)

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "46f1d48a7cf1956a"; }
+/******/ 		__webpack_require__.h = function() { return "b41c36e5fcb8b22f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -4,7 +4,7 @@ const AboutSection = () => {
   return (
     <section id="about" className="py-20 bg-black flex flex-col items-center justify-center gap-8">
         <h2 className="text-4xl font-bold text-white text-center">
-          About RE:INVENT
+          About <span className="text-gradient">RE:INVENT</span>
         </h2>
       <CardSpotlightDemo />
     </section>
