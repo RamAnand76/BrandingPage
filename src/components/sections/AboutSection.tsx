fe0d812d5f941a45553ba997e0b00269afdae2ee
Ramanand R <ramuttenravitsr2002@gmail.com>
2025-11-08 05:40:46 +0000

    whenever i search this name i get amazon reinvent or any brand named rei

--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -6,7 +6,7 @@ const AboutSection = () => {
     <section id="about" className="py-20 bg-black flex flex-col items-center justify-center gap-16">
         <div className="flex flex-col gap-8 items-center">
             <h2 className="text-4xl font-bold text-white text-center">
-            About <span className="text-gradient">RE:INVENT</span>
+            About <span className="text-gradient">Re-Invent</span>
             </h2>
             <CardSpotlightDemo />
         </div>
