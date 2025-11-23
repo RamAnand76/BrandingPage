8e179aa63aa20d8df353787b59ca088bc2a99078
Ramanand R <ramuttenravitsr2002@gmail.com>
2025-11-23 10:18:17 +0000

    Can you make the card component more premium and Aesthetically appealing

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -163,7 +163,7 @@
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/chunks/" + chunkId + ".js";
+/******/ 			return undefined;
 /******/ 		};
 /******/ 	}();
 /******/ 	
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eaf29ab8924eedbb"; }
+/******/ 		__webpack_require__.h = function() { return "b2f2975cd477d0ff"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
